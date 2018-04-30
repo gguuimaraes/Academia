64db88fc87b718d522bcce8ab008b0999ed5e462
Gabriel G <gguuimaraes@hotmail.com>
2018-04-30 11:26:25 -0300

    Update Sexo enum

--- src/main/java/br/com/pmgt/uteis/Sexo.java
+++ src/main/java/br/com/pmgt/uteis/Sexo.java
@@ -7,4 +7,9 @@ public enum Sexo {
 	public Sexo[] getSexos() {
 		return values();
 	}
+	
+	public String toString() {
+		String old = super.toString();
+		return old.substring(0, 1) + old.substring(1).toLowerCase();
+	}
 }
